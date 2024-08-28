04f402caa76738870526bfd2c84e2bfbc51b9a29
tianmaoyu <tianmaoyu@link-smart.cn>
2024-08-28 11:47:42 +0800

    xx

--- 13-model-pretrained-001.py
+++ 13-model-pretrained-001.py
@@ -1,4 +1,8 @@
+import os
+
+import json
 import numpy
+import requests
 import torch
 import torchvision.datasets
 from torch import nn
@@ -6,54 +10,43 @@ import torch.nn.functional as F
 from torch.nn import Conv2d
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
-from torchvision import datasets
+from torchvision import datasets, models
 from torchvision import transforms
 # import matplotlib.pyplot as plt
 from PIL import Image
 import matplotlib.pyplot as plt
 
-dataset = torchvision.datasets.CIFAR10("./dataset", train=True, download=True, transform=transforms.ToTensor())
-dataloader = DataLoader(dataset=dataset, batch_size=64)
+# 设置环境变量
+os.environ['TORCH_HOME'] = "./dataset"
+model = torchvision.models.vgg16(weights=models.VGG16_Weights.DEFAULT)
+model_false = models.vgg16(weights=None)
 
+preprocess = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+input_image = Image.open("data/test.jpg")
 
-class DemoModule(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.module1 = nn.Sequential(
-            nn.Conv2d(3, 32, 5, padding=2),
-            nn.MaxPool2d(kernel_size=(2, 2)),
-            nn.Conv2d(32, 32, 5, padding=2),
-            nn.MaxPool2d(kernel_size=(2, 2)),
-            nn.Conv2d(32, 64, 5, padding=2),
-            nn.MaxPool2d(kernel_size=(2, 2)),
-            nn.Flatten(),
-            nn.Linear(64 * 4 * 4, 64),
-            nn.Linear(64, 10)
-        )
+input_tensor = preprocess(input_image)
+input_tensor = input_tensor.reshape(1, *input_tensor.shape)
 
-    def forward(self, input):
-        output = self.module1(input)
-        return output
+if torch.cuda.is_available():
+    input_tensor = input_tensor.to('cuda')
+    model.to("cuda")
 
+with torch.no_grad():
+    output = model(input_tensor)
 
-loss = nn.CrossEntropyLoss()
-module = DemoModule()
-sgd = torch.optim.SGD(module.parameters(),lr=0.02)
+probabilities = torch.nn.functional.softmax(output[0], dim=0)
+print(probabilities)
 
+# 获取ImageNet的标签
+url = "https://raw.githubusercontent.com/anishathalye/imagenet-simple-labels/master/imagenet-simple-labels.json"
+labels = json.loads(requests.get(url).text)
 
-for epoch in range(20):
-    step=0
-    for data in dataloader:
-        images, targets = data
-        outputs = module(images)
-        result_loss= loss(outputs,targets)
-        sgd.zero_grad()
-        result_loss.backward()
-        sgd.step()
-
-        if step%100==0:
-            print(result_loss)
-        step += step
-
-
-
+# 打印前5个类别和对应的概率
+top5_prob, top5_catid = torch.topk(probabilities, 5)
+for i in range(top5_prob.size(0)):
+    print(labels[top5_catid[i]], top5_prob[i].item())
