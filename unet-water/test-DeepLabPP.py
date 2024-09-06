c8ee953be712a52a77d43ac9d26a33e4957a0022
tianmaoyu <tianmaoyu@link-smart.cn>
2024-09-06 16:54:38 +0800

    PSPNet +ResNet

--- unet-water/test-DeepLabPP.py
+++ unet-water/test-DeepLabPP.py
@@ -8,8 +8,9 @@ import  utils
 from  net_standard_v2 import UNetV2
 
 
-model_path="./water/PSPNet-28.pth"
+model_path="./water/DeepLapV3PP-4.pth"
 image_path="./water/DJI_20240423132549_0005_W.JPG"
+image_path="./water/DJI_20240423132515_0002_W.JPG"
 
 image = Image.open(image_path).convert("RGB")
 model= torch.load(model_path,map_location="cpu")
@@ -23,8 +24,9 @@ with torch.no_grad():
     image= image.unsqueeze(dim=0)
     image = utils.pad_16(image)
     # image = image.to('cuda')
+    model.eval()
     output= model(image)
-    torchvision.utils.save_image(output,fp="./water/5-28.jpg")
+    torchvision.utils.save_image(output,fp="./water/2-4.jpg")
     print("---"*8)
 
 
